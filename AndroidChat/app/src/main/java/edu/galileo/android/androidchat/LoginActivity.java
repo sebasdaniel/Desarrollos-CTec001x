e5112bc1d5a13f57d7ffda08be61a5e4c2c0cc07
sebasdaniel <pe.sebas@gmail.com>
2016-06-13 23:17:08 -0500

    configure and add butterknife anotations

--- AndroidChat/app/src/main/java/edu/galileo/android/androidchat/LoginActivity.java
+++ AndroidChat/app/src/main/java/edu/galileo/android/androidchat/LoginActivity.java
@@ -1,13 +1,46 @@
 package edu.galileo.android.androidchat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.editTxtEmail)
+    EditText inputEmail;
+    @Bind(R.id.editTxtPassword)
+    EditText inputPassword;
+    @Bind(R.id.btnSignin)
+    Button btnSignIn;
+    @Bind(R.id.btnSignup)
+    Button btnSignUp;
+    @Bind(R.id.progressBar)
+    ProgressBar progressBar;
+    @Bind(R.id.layoutMainContent)
+    RelativeLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnSignin)
+    public void handleSignIn() {
+        Toast.makeText(this, "click en SignIn", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.btnSignup)
+    public void handleSignUp() {
+        Toast.makeText(this, "click en SignUp", Toast.LENGTH_SHORT).show();
     }
 }
